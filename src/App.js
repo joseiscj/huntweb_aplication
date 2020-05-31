7e176312e51dfb2661725f7802de4ce80856cccc
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-05-31 17:34:56 -0300

    buscando produtos da api - pt2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import api from "./services/api";
+import "./styles.css";
 
 import Header from "./components/Header";
-import "./styles.css";
+import Main from "./pages/main";
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 )
-
 
 export default App;
