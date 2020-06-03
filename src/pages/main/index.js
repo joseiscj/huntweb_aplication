6190eba84e8009f2427f447a781bf2c5f6d935fd
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-06-02 21:47:37 -0300

    exibição da descrição do repo e estilização

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
+import "./styles.css";
+
 export default class Main extends Component {
     state = {
         products: [],
@@ -19,7 +21,12 @@ export default class Main extends Component {
     return (
         <div className="product-list">
             {this.state.products.map(product => (
-                <h2 key={product._id}>{product.title}</h2>
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                </article>
             ))}
         </div>
     )
